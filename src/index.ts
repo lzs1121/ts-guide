ae80178189ba3fdb83e85a1bc2400394baca79a4
Zishuo Li <zishuoli@Zishuos-MacBook-Pro.local>
2019-12-19 16:06:56 +1100

    1st commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { User } from './User';
+import { Company } from './Company';
+
+// const user = new User();
+// console.log(user);
+
+// const company = new Company();
+// console.log(company);
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
+
+this.map();
